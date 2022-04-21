01d23313496f1b8738b5f2a458c1674a34c56bba
a2mo <Ali.alimohammadi@yahoo.com>
2022-04-21 19:03:11 +0430

    [feature]Prepare module structure

---
+++ license-manager-license/src/main/java/ir/a2mo/licensemanager/license/LicenseServiceApplication.java
@@ -0,0 +1,14 @@
+package ir.a2mo.licensemanager.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LicenseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicenseServiceApplication.class, args);
+    }
+}
